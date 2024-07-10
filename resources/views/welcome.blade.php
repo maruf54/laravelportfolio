6d94b125e69b0eab89fb79360417dce750488e88
unknown <marufhossain5454@gmail.com>
2024-07-10 21:04:48 +0600

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,287 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Resume - Md Maruf Hossain</title>
+        <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico" />
+        <!-- Font Awesome icons (free version)-->
+        <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:500,700" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Muli:400,400i,800,800i" rel="stylesheet" type="text/css" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
+            <a class="navbar-brand js-scroll-trigger" href="#page-top">
+                <span class="d-block d-lg-none">Clarence Taylor</span>
+                <span class="d-none d-lg-block"><img class="img-fluid img-profile rounded-circle mx-auto mb-2" src="assets/img/profile.jpg" alt="..." /></span>
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav">
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#about">About</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#experience">Experience</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#education">Education</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#training">Training</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#skills">Skills</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#projects">Projects</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#interests">Interests</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#awards">Awards</a></li>
+                </ul>
+            </div>
+        </nav>
+        <!-- Page Content-->
+        <div class="container-fluid p-0">
+            <!-- About-->
+            <section class="resume-section" id="about">
+                <div class="resume-section-content">
+                    <h1 class="mb-0">
+                        MD MARUF 
+                        <span class="text-primary">HOSSAIN</span>
+                    </h1>
+                    <div class="subheading mb-5">
+                        Sadipur, Bondobila, Bagherpara, Jashore 7400, Contact:+8801738764243
+                        <a href="mailto:name@email.com"> maruf.phd.cse@email.com </a>
+                    </div>
+                    <p class="lead mb-5">My goal is to develop a career as a web developer in a national or multinational company. Striving to
+                        become a proficient and promising professional through skills, sincerity, responsibility, and honesty.
+                        My objective is to contribute effectively to an organization's goals by leveraging my abilities, skills,
+                        and knowledge.</p>
+                    <div class="social-icons">
+                        <a class="social-icon" href="https://www.linkedin.com/in/mdmarufhossain43/"><i class="fab fa-linkedin-in"></i></a>
+                        <a class="social-icon" href="https://github.com/maruf54"><i class="fab fa-github"></i></a>
+                        <a class="social-icon" href="https://x.com/marufhossain545"><i class="fab fa-twitter"></i></a>
+                        <a class="social-icon" href="https://www.facebook.com/Marufhossain43/"><i class="fab fa-facebook-f"></i></a>
+                    </div>
+                </div>
+            </section>
+            <hr class="m-0" />
+            <!-- Experience-->
+            <section class="resume-section" id="experience">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">Experience</h2>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">Web Developer (Laravel & WordPress)</h3>
+                            <div class="subheading mb-3">Fiver job market</div>
+                            <p>As a web developer specializing in Laravel and WordPress framworks on the Fiverr job market,
+                                 my responsibilities include designing and developing custom websites and web applications using Laravel, creating and customizing WordPress themes and plugins, ensuring responsive and user-friendly designs, debugging and troubleshooting issues, optimizing website performance, and providing ongoing maintenance and support.</p>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">December 2020 - Present</span></div>
+                    </div>
+
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">Chairperson</h3>
+                            <div class="subheading mb-3">IEEE GUB Student Branch</div>
+                            <p>As a Chairperson of  IEEE student branch GUB, My responsible was leading and representing the branch, organizing technical workshops and seminar, maintaining effective communication, developing membership, overseeing financial management, supporting projects, and ensuring compliance with IEEE regulations.</p>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">January 2022 - January 2023</span></div>
+                    </div>
+                    
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">Event Management intern</h3>
+                            <div class="subheading mb-3">GUB Center of Excellence in Teaching and Learning</div>
+                            <p>Evaluating the success of the event and identifying areas for improvement for future events. Developing a comprehensive plan for the events and creating and managing the budget for the event.
+                                Additinallu coordinating all aspects of the event on the day of the event, including managing vendors, overseeing set-up and teardown, and ensuring that everything runs smoothly.
+                                </p>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">October 2021 - January 2022</span></div>
+                    </div>
+                    
+                </div>
+            </section>
+            <hr class="m-0" />
+            <!-- Education-->
+            <section class="resume-section" id="education">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">Education</h2>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">Green University Of Bangladesh</h3>
+                            <div class="subheading mb-3">Bachelor of Science</div>
+                            <div>Computer Science and Engineering</div>
+                            <p>CGPA: 3.56 Out of 4.0</p>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">January 2019 - February 2023</span></div>
+                    </div>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">Hamidpur Al-hera Collage</h3>
+                            <div class="subheading mb-3"> Higher Secondary</div>
+                            <div> Science</div>
+                            <p>CGPA: 4.44 Out of 5.0</p>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">January 2015 - February 2017</span></div>
+                    </div>
+                    
+                </div>
+            </section>
+            <section class="resume-section" id="training">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">Training</h2>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0"> Bangladesh ICT Ministry-Learning & Earning Development Training Program</h3>
+                            <div class="subheading mb-3"> Web Design and Development</div>
+                    
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">July 2023 </span></div>
+                    </div>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">Green University IEEE Computer Society</h3>
+                            <div class="subheading mb-3"> Workshop on SQA & Test Automation </div>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">February 2022</span></div>
+                    </div>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">Bangladesh Technical Education Board </h3>
+                            <div class="subheading mb-3"> Computer Office Application </div>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">July 2013</span></div>
+                    </div>
+                </div>
+            </section>
+
+
+
+            <hr class="m-0" />
+            <!-- Skills-->
+            <section class="resume-section" id="skills">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">Skills</h2>
+                    <div class="subheading mb-3">Programming Languages & Tools</div>
+                    <ul class="list-inline dev-icons">
+                        <li class="list-inline-item"><i class="fab fa-html5"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-bootstrap"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-MySQL"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-php"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-js"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-laravel"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-wordpress"></i></li>
+                       
+                    </ul>
+                    <div class="subheading mb-3">Workflow</div>
+                    <ul class="fa-ul mb-0">
+                        <li>
+                            <span class="fa-li"><i class="fas fa-check"></i></span>
+                            Mobile-First, Responsive Design
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-check"></i></span>
+                            Cross Browser Testing & Debugging
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-check"></i></span>
+                            Cross Functional Teams
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-check"></i></span>
+                            Agile Development & Scrum
+                        </li>
+                    </ul>
+
+            
+                </div>
+            </section>
+            
+            <hr class="m-0" />
+            <!-- projects-->
+            <section class="resume-section" id="projects">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">Projects</h2>
+                   
+                    <ul class="fa-ul mb-0">
+                        <li>
+                            <span class="fa-li"><i class="fas fa-square"></i></span>
+                            Portfolio website (Laravel)
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-square"></i></span>
+                            Portfolio website (WordPress)
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-square"></i></span>
+                            Backend Admin Panel Project (PHP)
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-square"></i></span>
+                             Non-profit organization website development (Laravel)
+
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-square"></i></span>
+                             Carousel project (Java Script )
+
+                        </li>
+                        <a class="social-icon" href="https://github.com/maruf54"> Projects link <i class="fab fa-github"></i></a>
+
+                    </ul>
+
+
+
+
+
+            
+                </div>
+            </section>
+
+
+
+
+
+
+            <hr class="m-0" />
+            <!-- Interests-->
+            <section class="resume-section" id="interests">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">Interests</h2>
+                    <p>Apart from being a web developer, I enjoy most of my time being outdoors. In the winter, I am an avid skier and novice ice climber. During the warmer months, I enjoy mountain biking, free climbing, and kayaking.</p>
+                    <p class="mb-0">When forced indoors, I follow a number of sci-fi and fantasy genre movies and television shows, I am an aspiring chef, and I spend a large amount of my free time exploring the latest technology advancements in the front-end web development world.</p>
+                </div>
+            </section>
+            <hr class="m-0" />
+            <!-- Awards-->
+            <section class="resume-section" id="awards">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">Awards & Certifications</h2>
+                    <ul class="fa-ul mb-0">
+
+                        <li>
+                            <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
+                            1
+                            <sup>st</sup>
+                            Place -  GUB Intra Department Programming Contest  - Green University CSE carnival-2021
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
+                            1
+                            <sup>st</sup>
+                            Place - IEEE GUB Student Branch Chapter (Best Volunteer)
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
+                            Dean Award -
+                           
+                             Green University Of Bangladesh (Spring-2019)
+                        </li>
+                        
+                    </ul>
+                </div>
+            </section>
+        </div>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
